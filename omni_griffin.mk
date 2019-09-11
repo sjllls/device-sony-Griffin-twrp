66d9d9c8293245370482098710c13f5920309fbc
sjllls <2012ji123@gmail.com>
2019-09-11 11:50:56 +0800

    set up

---
+++ omni_griffin.mk
@@ -0,0 +1,48 @@
+#
+# Copyright 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := griffin
+
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/sony/griffin/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := griffin
+PRODUCT_NAME := omni_griffin
+PRODUCT_BRAND := Sony
+PRODUCT_MODEL := Xperia 1
+PRODUCT_MANUFACTURER := Sony
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=griffin \
+    BUILD_PRODUCT=griffin \
+    TARGET_DEVICE=griffin
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
+    ro.product.model \
+    ro.product.device \
+    ro.product.name \
+    ro.display.series
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
